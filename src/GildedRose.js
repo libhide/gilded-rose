04df06bc31bb84d170ea52c338cc0047a32b4200
Ratik Sharma <Ratik96@gmail.com>
2018-12-16 16:45:17 +0530

    Consolidate a few more conditionals

--- src/GildedRose.js
+++ src/GildedRose.js
@@ -19,15 +19,11 @@ class GildedRose {
         if (item.quality < 50) {
           item.quality = item.quality + 1;
           if (item.name == 'Backstage passes to a TAFKAL80ETC concert') {
-            if (item.sellIn < 11) {
-              if (item.quality < 50) {
-                item.quality = item.quality + 1;
-              }
+            if (item.sellIn < 11 && item.quality < 50) {
+              item.quality = item.quality + 1;
             }
-            if (item.sellIn < 6) {
-              if (item.quality < 50) {
-                item.quality = item.quality + 1;
-              }
+            if (item.sellIn < 6 && item.quality < 50) {
+              item.quality = item.quality + 1;
             }
           }
         }
