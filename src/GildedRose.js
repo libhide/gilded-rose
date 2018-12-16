06b31ce3746cd8538964c61ba5cde1d4831af657
Ratik Sharma <Ratik96@gmail.com>
2018-12-17 01:18:09 +0530

    Extract function that receives a generic item object and returns a typed object

--- src/GildedRose.js
+++ src/GildedRose.js
@@ -18,30 +18,24 @@ class GildedRose {
     this._items = aList;
   }
 
+  _createItem(item) {
+    switch (item.name) {
+      case 'Aged Brie':
+        return new AgedBrieItem(item.name, item.sellIn, item.quality);
+      case 'Sulfuras, Hand of Ragnaros':
+        return new SulfurasItem(item.name, item.sellIn, item.quality);
+      case 'Backstage passes to a TAFKAL80ETC concert':
+        return new BackstageItem(item.name, item.sellIn, item.quality);
+      default:
+        return new DefaultItem(item.name, item.sellIn, item.quality);
+    }
+  }
+
   tick() {
-    this.items = this.items.map(item => {
-      switch (item.name) {
-        case 'Aged Brie': {
-          const agedBrieItem = new AgedBrieItem(item.name, item.sellIn, item.quality);
-          agedBrieItem.tick();
-          return agedBrieItem;
-        }
-        case 'Sulfuras, Hand of Ragnaros': {
-          const sulfurasItem = new SulfurasItem(item.name, item.sellIn, item.quality);
-          sulfurasItem.tick();
-          return sulfurasItem;
-        }
-        case 'Backstage passes to a TAFKAL80ETC concert': {
-          const backstageItem = new BackstageItem(item.name, item.sellIn, item.quality);
-          backstageItem.tick();
-          return backstageItem;
-        }
-        default: {
-          const defaultItem = new DefaultItem(item.name, item.sellIn, item.quality);
-          defaultItem.tick();
-          return defaultItem;
-        }
-      }
+    this.items = this.items.map(i => {
+      const item = this._createItem(i);
+      item.tick();
+      return item;
     });
   }
 }
