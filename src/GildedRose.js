3f83aaa5b7fbd18aa30d84c6752116e00fdb382b
Ratik Sharma <Ratik96@gmail.com>
2018-12-16 18:49:39 +0530

    Remove the dead conditional code

--- src/GildedRose.js
+++ src/GildedRose.js
@@ -68,46 +68,6 @@ class GildedRose {
       } else {
         // not-implemented yet
       }
-
-      if (item.name != 'Aged Brie' && item.name != 'Backstage passes to a TAFKAL80ETC concert') {
-        if (item.quality > 0 && item.name != 'Sulfuras, Hand of Ragnaros') {
-          item.quality = item.quality - 1;
-        }
-      } else {
-        if (item.quality < 50) {
-          item.quality = item.quality + 1;
-          if (item.name == 'Backstage passes to a TAFKAL80ETC concert') {
-            if (item.sellIn < 11 && item.quality < 50) {
-              item.quality = item.quality + 1;
-            }
-            if (item.sellIn < 6 && item.quality < 50) {
-              item.quality = item.quality + 1;
-            }
-          }
-        }
-      }
-
-      if (item.name != 'Sulfuras, Hand of Ragnaros') {
-        item.sellIn = item.sellIn - 1;
-      }
-
-      if (item.sellIn < 0) {
-        if (item.name != 'Aged Brie') {
-          if (item.name != 'Backstage passes to a TAFKAL80ETC concert') {
-            if (item.quality > 0) {
-              if (item.name != 'Sulfuras, Hand of Ragnaros') {
-                item.quality = item.quality - 1;
-              }
-            }
-          } else {
-            item.quality = item.quality - item.quality;
-          }
-        } else {
-          if (item.quality < 50) {
-            item.quality = item.quality + 1;
-          }
-        }
-      }
     }
   }
 }
